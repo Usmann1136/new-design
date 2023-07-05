c9b85aa6c118c527bebdf36405dedabd62db9075
Usmann1136 <121626247+Usmann1136@users.noreply.github.com>
2023-07-05 17:16:25 +0500

    Update app_bar.dart

--- lib/Module/Splash/Auth/View/Components/app_bar.dart
+++ lib/Module/Splash/Auth/View/Components/app_bar.dart
@@ -1,29 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:tour_easy2/Module/Registration/Pending/View/Components/custom_sizedbox.dart';
 import 'package:tour_easy2/Module/Splash/View/Components/splash_title.dart';
 import 'package:tour_easy2/Utils/theme/colors.dart';
+import 'package:tour_easy2/Utils/theme/size.dart';
 
 import '../../../../Utils/theme/font_style.dart';
 
-PreferredSizeWidget appbar(BuildContext context , String text , {VoidCallback? onPressed}   ){
-return AppBar(backgroundColor: AppColor.scaffoldcolor,
-elevation: 0,
+class PrimaryCustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  
+final Widget leading;
+final List<Widget> actions;
+final Color titlecolor;
+ final String? title ;
+  const PrimaryCustomAppBar({
+    required this.leading,
+    required this.actions,
+    required this.titlecolor,
+    this.title,
+  });
 
-leading: IconButton(onPressed: (){
-Get.back();
-}, icon: Icon(Icons.chevron_left , color: AppColor.blackcolor,)),
-title: textWidget('Tour Ease', AppColor.splashtitlecolor, TextStyles.fontWeight500, TextStyles.fontSize24
-, TextAlign.center, TextStyles.ubuntumedium),
-centerTitle: true,
-actions: [
-  TextButton(onPressed: onPressed, child: Text(text ,
-  textAlign: TextAlign.right,
-   style: TextStyle(
-    color: AppColor.blackcolor,
-    
-    decoration: TextDecoration.underline,
-    fontWeight: TextStyles.fontWeight400,
-    fontSize: TextStyles.fontSize14 , fontFamily: TextStyles.ubunturegular),) )
-],
-);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Container(
+      height: kToolbarHeight,
+      color: AppColor.scaffoldcolor,
+      child: Row(
+      
+       
+      
+      children: [
+      
+      
+      if (leading != null) leading,
+              
+      customsizedbox(context , width: Sizes.size50),
+             Expanded(
+               child: Align(
+                alignment: Alignment.center,
+                 child: textWidget(title!, titlecolor, TextStyles.fontWeight500, TextStyles.fontSize20
+                 
+                 , TextAlign.center, TextStyles.ubuntumedium),
+               ),
+             ),
+      
+      
+      if (actions != null) ...actions
+            
+       
+            
+             
+      
+      
+      
+      ],),
+      
+       
+      
+      
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
