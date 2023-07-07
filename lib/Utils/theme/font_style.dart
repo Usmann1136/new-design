dc4a883824db294183b799c4713fbf4109323b04
Usmann1136 <121626247+Usmann1136@users.noreply.github.com>
2023-07-07 16:48:59 +0500

    Update font_style.dart

--- lib/Utils/theme/font_style.dart
+++ lib/Utils/theme/font_style.dart
@@ -1,6 +1,24 @@
 
 
+
+import 'package:flutter/material.dart';
+
 class TextStyles{
-   static const String ubuntubold = 'Ubuntu Bold';
-   static const String ubunturegular = 'Ubuntu Regular';
+    static const String ubuntubold = 'Ubuntu Bold';
+    static const String ubunturegular = 'Ubuntu Regular';
+    static const String ubuntumedium = 'Ubuntu Medium';
+    static const String ubuntulight = 'Ubuntu Light';
+    static const FontWeight fontWeight300  = FontWeight.w300;
+    static const FontWeight fontWeight400  = FontWeight.w400;
+    static const FontWeight fontWeight500  = FontWeight.w500;
+    static const double fontSize24  = 24;
+    static const double fontSize20  = 20;
+    static const double fontSize12  = 12;
+    static const double fontSize13  = 13;
+    static const double fontSize10  = 10;
+    static const double fontSize14  = 14;
+    static const double fontSize18  = 18;
+    static const double fontSize16  = 16;
+    static const double fontSize8  = 8;
+
 }
