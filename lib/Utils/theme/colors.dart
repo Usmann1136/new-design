5d78ae75cc9e126fdf34e1329c7e0d1b843d2597
Usmann1136 <121626247+Usmann1136@users.noreply.github.com>
2023-07-07 16:50:20 +0500

    Update colors.dart

--- lib/Utils/theme/colors.dart
+++ lib/Utils/theme/colors.dart
@@ -1,13 +1,23 @@
 
+
 import 'package:flutter/material.dart';
 
 class AppColor{
   static const Color splashtitlecolor = Color(0xff16A0A5);
   static const Color attoureasycolor = Color(0xff666666);
   static const Color blackcolor = Color(0xff121212);
-  static const Color secondaryblackcolor = Color(0xff000000);
+  static const Color secondaryblackcolor = Color(0xff111111);
+  static const Color thirdblackcolor = Color(0xff000000);
+  static const Color fourthblackcolor = Color(0xff1B1B1B);
   static const Color activedotcolor = Color(0xffD9D9D9);
+  static const Color applelogocolor = Color(0xff9D9D9D);
+  static const Color imagetilecolor = Color(0xffBEBCBC);
   static const Color inactivedotcolor = Color(0xff666666);
+  static const Color budgetcolor = Color(0xffC7C7C7);
+  static const Color bottombarcolor = Color(0xffF5F5F5);
+  static const Color ambercolor = Color(0xffFFCE31);
+  static const Color logoutcolor = Color(0xffFB562C);
+    static const Color morecolor = Color(0xff484848);
   static const Color scaffoldcolor = Colors.white;
+  static const Color transparentcolor = Colors.transparent;
 
-}
